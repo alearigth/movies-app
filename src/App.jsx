74c976d570c76497a3d216de625f9d3b35a272e5
alearigth <91171514+alearigth@users.noreply.github.com>
2022-07-06 02:26:27 -0500

    primera parte

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MoviesGrid from "./MoviesGrid"
+import styles from "./App.module.css"
+
+function App() {
+    return <div>
+        <header>
+            <h1 className={styles.title}>Films</h1>
+        </header>
+        <main>
+            <MoviesGrid />
+        </main>
+    </div>
+}
+
+export default App
