e43d533cf8fbb1f7573b8ff2237b60b7445f8d3f
alearigth <91171514+alearigth@users.noreply.github.com>
2022-07-07 23:07:25 -0500

    agregados

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,14 @@
 import MoviesGrid from "../components/MoviesGrid";
+import { Search } from "../components/Search";
 
 function LandingPage() {
-  return <MoviesGrid />;
+  return (
+    <div>
+      <Search />
+
+      <MoviesGrid />
+    </div>
+  );
 }
 
 export default LandingPage;
